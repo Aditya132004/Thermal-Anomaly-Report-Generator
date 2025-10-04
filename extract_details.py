412eeb895b8106a14d7e2c125723e6a2779defff
Aditya132004 <101162163+Aditya132004@users.noreply.github.com>
2025-10-04 12:49:29 +0530

    Delete extract_details.py

--- extract_details.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-
-thermal_img = cv2.imread("images/thermal/anomaly1_thermal.jpg", cv2.IMREAD_GRAYSCALE)
-avg_temp = np.mean(thermal_img)   # average pixel intensity
-max_temp = np.max(thermal_img)    # hottest pixel
-min_temp = np.min(thermal_img)    # coolest pixel
-
-print("Min:", min_temp, "Max:", max_temp, "Avg:", avg_temp)
